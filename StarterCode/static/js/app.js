5d38545da845eda68a9b20c5973da1cf7ef17822
Master-Leo <emillio3198@gmail.com>
2022-10-27 17:56:52 -0700

    'fixed layout'

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,28 +1,28 @@
 function buildMetadata(sample){
-    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-        let metadata = data.metadata;
-        // Filter the data for the object with the desired sample number
-    ​    let metaObject = metadata.filter((sampleObject) => sampleObject.id == sample)[0];
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+    let metadata = data.metadata;
+    // Filter the data for the object with the desired sample number
+​    let metaObject = metadata.filter((sampleObject) => sampleObject.id == sample)[0];
 
-        // Use d3 to select the panel with id of `#sample-metadata`
-        let sampleMeta = d3.select('#sample-metadata');
-    ​
-        // Use `.html("") to clear any existing metadata
-        sampleMeta.html('');
-    ​=
-        // Hint: Inside the loop, you will need to use d3 to append new
-        // tags for each key-value in the metadata.
-    ​    Object.entries(metaObject).forEach(([key, value]) => {
-            sampleMeta.append("h5").text(`${key}: ${value}`);
+    // Use d3 to select the panel with id of `#sample-metadata`
+    let sampleMeta = d3.select('#sample-metadata');
+​
+    // Use `.html("") to clear any existing metadata
+    sampleMeta.html('');
+​=
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+​    Object.entries(metaObject).forEach(([key, value]) => {
+        sampleMeta.append("h5").text(`${key}: ${value}`);
 
-        });
-        // BONUS: Build the Gauge Chart ... give this a try if you have time.  Otherwise don't add anything.
-        
-      });
-    }
-      function buildCharts(sample) {
-        d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-        //   put the data into a variable
+    });
+    // BONUS: Build the Gauge Chart ... give this a try if you have time.  Otherwise don't add anything.
+    
+    });
+}
+    function buildCharts(sample) {
+      d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+       //   put the data into a variable
         let sample = data.sample;
 
         //   filter the data using 'sample'
@@ -30,12 +30,12 @@ function buildMetadata(sample){
 
         //   grab the first entry [0]
         let result = sampFilter[0];
-      
-          let otu_ids = result.otu_ids;
-          let otu_labels = result.otu_labels;
-          let sample_values = result.sample_values;
-      
-          // Build a Bubble Chart
+    
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+    
+        // Build a Bubble Chart
         //   https://plotly.com/javascript/bubble-charts/
 
         // create a trace for chart 
@@ -49,7 +49,7 @@ function buildMetadata(sample){
                 size: sample_values,
                 coloscale: 'Jet'
             }
-        }
+        };
 
         let bubbleLayout = [bubble]
 
@@ -57,30 +57,36 @@ function buildMetadata(sample){
         var bubblelayout = {
             xaxis: { title: 'OTU ID'},
             margin: { t: 50 },
-        }
-      
-          Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-      
+        };
+    
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    
         // slice the data down to 10 items  
         //you will probably want to reverse them to get them into desc order
         let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
-          //create trace
-          var bar1 = [{
-                x: sample_values.slice(0,10).reverse(),
-                y: yticks,
-                text: otu_labels.slice(0,10).reverse(),
-                type: 'bar',
-                orientation: 'h',
-          }];
-        
-            // create layout  (title is enough)
-    ​
-            // draw your plot Plotly.newPlot()
-          
+        //create trace
+        var trac1 = [{
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks,
+            text: otu_labels.slice(0,10).reverse(),
+            type: 'bar',
+            orientation: 'h',
+        }];
+    
+        var data = [trace1]
+
+        // create layout  (title is enough)
+        var layout = {
+            title: '<b>Top 10 Bacteria found per individual'
+        };
+​
+        // draw your plot Plotly.newPlot()
+        Plotly.newPlot('bar',data, layout);
+
         });
-      }
-      
+    }
+    
       function init(){
         //use d3 to select the dropdown element ($selDataset)
     ​
