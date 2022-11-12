4c440b46604af6527970cf6add1065c808393420
Master-Leo <emillio3198@gmail.com>
2022-11-11 20:01:03 -0800

    'updated functional charts'

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,40 +1,44 @@
 function buildMetadata(sample){
-d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
-    let metadata = data.metadata;
-    // Filter the data for the object with the desired sample number
-​    let metaObject = metadata.filter((sampleObject) => sampleObject.id == sample)[0];
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+        let metadata = data.metadata;
+        // Filter the data for the object with the desired sample number
+        var metaObject = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = metaObject[0];
 
-    // Use d3 to select the panel with id of `#sample-metadata`
-    let sampleMeta = d3.select('#sample-metadata');
-​
-    // Use `.html("") to clear any existing metadata
-    sampleMeta.html('');
-​=
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-​    Object.entries(metaObject).forEach(([key, value]) => {
-        sampleMeta.append("h5").text(`${key}: ${value}`);
-
-    });
+        // Use d3 to select the panel with id of `#sample-metadata`
+        var sampleMeta = d3.select('#sample-metadata');
+        // Use `.html("") to clear any existing metadata
+        sampleMeta.html("");
+            
+        // Hint: Inside the loop, you will need to use d3 to append new
+        // tags for each key-value in the metadata.
+        Object.entries(result).forEach(([key, value]) => {
+            sampleMeta.append("h5").text(`${key}: ${value}`);
+        });
     // BONUS: Build the Gauge Chart ... give this a try if you have time.  Otherwise don't add anything.
-    
     });
 }
     function buildCharts(sample) {
       d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
        //   put the data into a variable
-        let sample = data.sample;
+        var Array = data.samples;
+        console.log(Array)
 
         //   filter the data using 'sample'
-        let sampFilter = sample.filter(sampleObject => sampleObject.id == sample);
+        var sampFilter = Array.filter(sampleObj => sampleObj.id == sample);
+        console.log(sampFilter)
 
         //   grab the first entry [0]
-        let result = sampFilter[0];
+        var result = sampFilter[0]
+        console.log(result)
     
-        let otu_ids = result.otu_ids;
-        let otu_labels = result.otu_labels;
-        let sample_values = result.sample_values;
-    
+        var otu_ids = result.otu_ids
+        var otu_labels = result.otu_labels
+        var sample_values = result.sample_values
+        console.log(otu_ids)
+        console.log(otu_labels)
+        console.log(sample_values)
+
         // Build a Bubble Chart
         //   https://plotly.com/javascript/bubble-charts/
 
@@ -44,66 +48,87 @@ d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1
             y: sample_values,
             text: otu_labels,
             mode: 'markers',
-            markers: {
-                color: otu_ids,
+            marker: {
                 size: sample_values,
-                coloscale: 'Jet'
+                color: otu_ids,
+                colorscale: 'Jet'
             }
         };
 
-        let bubbleLayout = [bubble]
+        let bubbleData = [bubble]
 
         // Create a layout 
         var bubblelayout = {
             xaxis: { title: 'OTU ID'},
-            margin: { t: 50 },
         };
     
-        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+        Plotly.newPlot('bubble', bubbleData, bubblelayout);
     
         // slice the data down to 10 items  
         //you will probably want to reverse them to get them into desc order
         let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        console.log(yticks)
 
         //create trace
-        var trac1 = [{
+        var trace1 = {
             x: sample_values.slice(0,10).reverse(),
             y: yticks,
             text: otu_labels.slice(0,10).reverse(),
             type: 'bar',
             orientation: 'h',
-        }];
+            marker: {
+                color: 'rgba(58,200,225,.5)',
+                line: {
+                  color: 'rgb(8,48,107)',
+                  width: 1.5
+                }
+            }
+        };
     
         var data = [trace1]
 
         // create layout  (title is enough)
         var layout = {
-            title: '<b>Top 10 Bacteria found per individual'
+            title: '<b>Top 10 Bacteria found per individual<b>'
         };
-​
         // draw your plot Plotly.newPlot()
-        Plotly.newPlot('bar',data, layout);
+        Plotly.newPlot('bar', data, layout);
 
         });
     }
-    
-      function init(){
+
+    function init(){
+        d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then((data) => {
+
         //use d3 to select the dropdown element ($selDataset)
-    ​
+            var dropDown = d3.select('#selDataset'); 
         // Use the list of sample names to populate the select options
-    ​
+            var sampleNames = data.names;
+
         //loop through names from sample names
         //append option 
-    ​
+            sampleNames.forEach((sample) =>{
+                dropDown
+                .append('option')
+                .text(sample)
+                .property('value',sample);
+            });
+
         // use the first sample from the list to build the intial plots
+            var sampleOne = sampleNames[0];
         // run build charts
+            buildCharts(sampleOne);
         // run build metadata
-      }
-    ​
-      function optionChanged(newSample){
+            buildMetadata(sampleOne);
+        });
+    }
+
+    function optionChanged(newSample){
         //Fetch new data each time a row sample is selected
         // run build charts
+        buildCharts(newSample);
         // run build metadata
-      }
-    ​
-      init()
+        buildMetadata(newSample);
+    }
+
+    init()
